a15efd08c2b087e642f594c8eefd07e5302baee3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-19 20:37:17 +0800

    add biological characters

--- src/Darwinism/BiologyCharacter.vb
+++ src/Darwinism/BiologyCharacter.vb
@@ -1,10 +1,26 @@
 ﻿Public Class BiologyCharacter
 
-    Public Property Character As BiologyCharacters
+    Public ReadOnly Property Character As BiologyCharacters
     Public Property Level As Double
+
+    Friend Shared ReadOnly all_characters As BiologyCharacters() = Enums(Of BiologyCharacters)()
+
 
 End Class
 
 Public Enum BiologyCharacters
-
+    None
+    ''' <summary>
+    ''' body size(weight)
+    ''' </summary>
+    BodySize
+    Wing
+    Claw
+    Tooth
+    OuterShell
+    Toxin
+    Antitoxin
+    ElectricalShock
+    FishFin
+    FishGill
 End Enum
--- src/Darwinism/Creature.vb
+++ src/Darwinism/Creature.vb
@@ -1,12 +1,30 @@
 ﻿Public Class Creature
 
     Public Property BiologicalCharacters As BiologyCharacter()
+        Get
+
+        End Get
+        Set(value As BiologyCharacter())
+
+        End Set
+    End Property
+
+    Dim index As Dictionary(Of BiologyCharacters, BiologyCharacter)
 
     Public Function Similarity(another As Creature) As Double
         If another Is Me Then
             Return 1
+        ElseIf another Is Nothing Then
+            Return 0
         End If
 
+        Dim x As Double() = GetVector()
+        Dim y As Double() = another.GetVector
+
+        Return Microsoft.VisualBasic.Math.SSM_SIMD(x, y)
+    End Function
+
+    Public Function GetVector() As Double()
 
     End Function
 
