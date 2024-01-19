b29f36c0c22519ebc4be8c38088acffbb7817b84
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-20 00:30:34 +0800

    data file

---
+++ src/Darwinism/DataFile/DataReader.vb
@@ -0,0 +1,3 @@
+﻿Public Class DataReader
+
+End Class
---
+++ src/Evolution/DataFile.vb
@@ -0,0 +1,28 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("Analysis")>
+Public Module DataFile
+
+    ''' <summary>
+    ''' open the simulation result file
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("open")>
+    Public Function open(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
+        Dim buf = SMRUCC.Rsharp.GetFileStream(file, IO.FileAccess.Read, env)
+
+        If buf Like GetType(Message) Then
+            Return buf.TryCast(Of Message)
+        End If
+
+
+    End Function
+
+End Module
