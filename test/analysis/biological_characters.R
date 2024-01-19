15bfe2f0b3e32084e80b00413a47d8e9560e65c1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-20 01:20:23 +0800

    export result

--- test/analysis/biological_characters.R
+++ test/analysis/biological_characters.R
@@ -5,4 +5,7 @@ imports "Analysis" from "Evolution";
 let result = Analysis::open(file = `${@dir}/../demo.dat`);
 let characters = Analysis::biology_abundance(result);
 
-print(characters);
+rownames(characters) = 1:nrow(characters);
+print(characters, max.print = 6);
+
+write.csv(characters, file = `${@dir}/biological_characters.csv`);
