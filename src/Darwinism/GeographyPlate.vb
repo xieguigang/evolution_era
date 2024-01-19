91c911977479deb93b48587a49606178e07fc89e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-19 21:08:01 +0800

    add point model

--- src/Darwinism/GeographyPlate.vb
+++ src/Darwinism/GeographyPlate.vb
@@ -1,11 +1,21 @@
+Imports Microsoft.VisualBasic.Data.GraphTheory.GridGraph
+Imports Microsoft.VisualBasic.Imaging
+
 Public Class GeographyPlate
 
-    Dim spatial
+    Dim spatial As Spatial3D(Of Position)
+
+    Sub New()
+
+    End Sub
 
 End Class
 
-Public Class Position
+Public Class Position : Implements IPoint3D
 
     Public Property Creature As Creature
+    Public Property Z As Integer Implements IPoint3D.Z
+    Public Property X As Integer Implements RasterPixel.X
+    Public Property Y As Integer Implements RasterPixel.Y
 
 End Class
