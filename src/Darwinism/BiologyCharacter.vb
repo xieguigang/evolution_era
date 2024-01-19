2979743c026e0edd928711884dd34f569e043b35
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-19 20:26:12 +0800

    add data models

---
+++ src/Darwinism/BiologyCharacter.vb
@@ -0,0 +1,10 @@
+﻿Public Class BiologyCharacter
+
+    Public Property Character As BiologyCharacters
+    Public Property Level As Double
+
+End Class
+
+Public Enum BiologyCharacters
+
+End Enum
---
+++ src/Darwinism/Creature.vb
@@ -0,0 +1,13 @@
+﻿Public Class Creature
+
+    Public Property BiologicalCharacters As BiologyCharacter()
+
+    Public Function Similarity(another As Creature) As Double
+        If another Is Me Then
+            Return 1
+        End If
+
+
+    End Function
+
+End Class
