64b93c4db271a9fe4bfb7bd5caf1191a3c70e1f6
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-21 00:59:03 +0800

    Update run_demo.R

--- test/run_demo.R
+++ test/run_demo.R
@@ -3,9 +3,9 @@ require( evolution_era);
 
 let worldModel = Era::world(defaultWorldMap(),
     reproductive.isolation = 0.8,
-                          reproduce.rate = 0.45,
+                          reproduce.rate = 0.5,
                           dna.size = 6,
-                          natural.death = 30);
+                          natural.death = 50);
 let savefile = file(`${@dir}/demo.dat`, truncate = TRUE);
 
 Era::evolve(worldModel, file = savefile, time = 1000);
