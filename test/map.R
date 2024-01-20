061f1255c11ccbe146d01071d86382907cbbfa3d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-21 02:57:54 +0800

    Create map.R

---
+++ test/map.R
@@ -0,0 +1,13 @@
+require(evolution_era);
+
+imports "Analysis" from "Evolution";
+
+setwd(@dir);
+
+let result = Analysis::open(file = "./demo.dat");
+
+for(era in tqdm(0:1000)) {
+    bitmap(file = `./dist_maps/${era}.png`) {
+        distributionMap(result, era);
+    }
+}
