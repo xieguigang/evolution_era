bd03ed7562176124493ca068c37e9a9dc840452d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-16 22:09:20 +0800

    run packlage msbuild

--- man/Evolution/Analysis/Analysis.md
+++ man/Evolution/Analysis/Analysis.md
@@ -1,4 +1,4 @@
-# Analysis
+﻿# Analysis
 
 
 
--- test/run_demo.R
+++ test/run_demo.R
@@ -2,12 +2,12 @@ require( evolution_era);
 
 
 let worldModel = Era::world(defaultWorldMap(),
-    reproductive.isolation = 0.8,
-                          reproduce.rate = 0.45,
-                          dna.size = 6,
-                          natural.death = 30);
+    reproductive.isolation = 0.85,
+                          reproduce.rate = 0.25,
+                          dna.size = 8,
+                          natural.death = 45);
 let savefile = file(`${@dir}/demo.dat`, truncate = TRUE);
 
-Era::evolve(worldModel, file = savefile, time = 1000);
+Era::evolve(worldModel, file = savefile, time = 8000);
 
 close(savefile);
